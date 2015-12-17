1baeee7912c1b8390a8936bcaa6157de42de2cfe
Marshall <mwagner273@gmail.com>
2015-12-17 05:48:19 -0500

    added asteroids and shit

--- src/Environment/Asteroids.java
+++ src/Environment/Asteroids.java
@@ -8,6 +8,7 @@ import com.jogamp.opengl.util.texture.Texture;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.Random;
 
 /**
  * Created by rodge on 12/16/2015.
@@ -15,27 +16,73 @@ import java.nio.file.Paths;
 public class Asteroids {
 
 
-    public static GLModel astOBJ = null;
-    public static Texture temp = null;
 
+    private static Texture temp = null;
+    private static Asteroids[] asteroids;
+    private static int maxAsteroids;
+    private float posX, posY, posZ, vecX,vecY,vecZ,rotX,rotY,rotZ;
+    private GLModel model;
 
-    public static Boolean loadModels(GL2 gl) {
-        temp = TextureLoader.loadTexture(
-                new File(Paths.get(".\\Models").toAbsolutePath().normalize().toString() +
-                        "\\Maps\\SmoothRock.jpg"));
-
-        astOBJ = ModelLoaderOBJ.LoadModel(Paths.get(".\\Models").toAbsolutePath().normalize().toString() + "\\Asteroid.obj",Paths.get(".\\Models").toAbsolutePath().normalize().toString() + "\\Asteroid.mtl",gl);
-        if (astOBJ == null) {
-            return false;
-        }
-        return true;
+    Asteroids(float posX, float posY,float posZ,float vecX,float vecY,float vecZ,float rotX,float rotY,float rotZ, GLModel model)
+    {
+        this.posX=posX; this.posY=posY; this.posZ=posZ;
+        this.vecX=vecX; this.vecY=vecY; this.vecZ=vecZ;
+        this.rotX=rotX; this.rotY=rotY; this.rotZ=rotZ;
+        this.model=model;
     }
 
-    public static void drawAsteroid(GL2 gl, float r, int longs, int lats){
+    public static void initAsteroids(GL2 gl, int numAsteroids){
+        loadTextures();
+        maxAsteroids = numAsteroids;
+        asteroids = new Asteroids[maxAsteroids];
+        int max = 15;
+        int min = 1;
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+
+        for(int i = 0; i < maxAsteroids; i++){
+            float randX = rand1.nextInt(2*max)-max;
+            float randY = rand1.nextInt(2*max)-max;
+            float randZ = rand1.nextInt(2*max)-max;
+            int randModel = rand2.nextInt(5)+1;
+            GLModel tempModel= randomModel(gl, randModel);
+            asteroids[i] = new Asteroids(
+                    randX,randY,randZ,
+                    0,0,0,
+                    0,0,0,
+                    tempModel);
+        }
+        int j = 0;
+
+    }
+
+
+    public static void  loadTextures() {
+        temp = TextureLoader.loadTexture(
+                new File(Paths.get(".\\Models").toAbsolutePath().normalize().toString() +
+                        "\\Maps\\PerforatedRock.jpg"));
+    }
+
+    public static GLModel randomModel(GL2 gl, int rand)
+    {
+        return ModelLoaderOBJ.LoadModel(
+                Paths.get(".\\Models").toAbsolutePath().normalize().toString()
+                        + "\\Asteroid\\Asteroid_"+rand+".obj",
+                Paths.get(".\\Models").toAbsolutePath().normalize().toString()
+                        + "\\Asteroid\\Asteroid_"+rand+".mtl",gl);
+
+    }
+
+
+    public static void drawAsteroidField(GL2 gl){
 
         temp.enable(gl);
         temp.bind(gl);
-        astOBJ.opengldraw(gl);
+        for(int i = 0; i < maxAsteroids; i++) {
+            gl.glTranslatef(asteroids[i].posX,asteroids[i].posY,asteroids[i].posZ);
+            asteroids[i].model.opengldraw(gl);
+            gl.glTranslatef(-asteroids[i].posX,-asteroids[i].posY,-asteroids[i].posZ);
+        }
         temp.disable(gl);
     }
 }
