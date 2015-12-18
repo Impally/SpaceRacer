57579e06842f56dbd792c4702da6b6f835456664
Marshall <mwagner273@gmail.com>
2015-12-17 20:05:14 -0500

    added random rotation value on asteroid init

--- src/Environment/Asteroids.java
+++ src/Environment/Asteroids.java
@@ -38,24 +38,17 @@ public class Asteroids {
         loadTextures();
         maxAsteroids = numAsteroids;
         asteroids = new Asteroids[maxAsteroids];
-        int max = 15;
-        int min = 1;
-        Random rand1 = new Random();
-        Random rand2 = new Random();
+
 
         for(int i = 0; i < maxAsteroids; i++){
-            float randX = rand1.nextInt(2*max)-max;
-            float randY = rand1.nextInt(2*max)-max;
-            float randZ = rand1.nextInt(2*max)-max;
-            int randModel = rand2.nextInt(5)+1;
-            GLModel tempModel= randomModel(gl, randModel);
+
+
             asteroids[i] = new Asteroids(
-                    randX,randY,randZ,
+                    Rand.randInt(0,15),Rand.randInt(0,15),Rand.randInt(0,15),
                     0,0,0,
-                    0,0,0,
-                    tempModel,loadTextures());
+                    Rand.randInt(0,5),Rand.randInt(0,5),Rand.randInt(0,5),
+                    randomModel(gl),loadTextures());
         }
-        int j = 0;
 
     }
 
@@ -68,7 +61,7 @@ public class Asteroids {
 
     }
 
-    public static GLModel randomModel(GL2 gl, int rand)
+    public static GLModel randomModel(GL2 gl)
     {
         return ModelLoaderOBJ.LoadModel(
                 Paths.get(".\\Models").toAbsolutePath().normalize().toString()
@@ -86,9 +79,11 @@ public class Asteroids {
         for(int i = 0; i < maxAsteroids; i++) {
             asteroids[i].tex.enable(gl);
             asteroids[i].tex.bind(gl);
-            gl.glTranslatef(asteroids[i].posX,asteroids[i].posY,asteroids[i].posZ);
+            gl.glTranslatef(asteroids[i].posX, asteroids[i].posY, asteroids[i].posZ);
+
             asteroids[i].model.opengldraw(gl);
-            gl.glTranslatef(-asteroids[i].posX,-asteroids[i].posY,-asteroids[i].posZ);
+
+            gl.glTranslatef(-asteroids[i].posX, -asteroids[i].posY, -asteroids[i].posZ);
             asteroids[i].tex.disable(gl);
         }
     }
