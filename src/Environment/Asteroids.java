fa115e2e03c8a05ffa712a303e9b4159cac53f7d
Marshall <mwagner273@gmail.com>
2015-12-17 19:41:59 -0500

    ASTEROIDS BABY

--- src/Environment/Asteroids.java
+++ src/Environment/Asteroids.java
@@ -2,6 +2,7 @@ package Environment;
 
 import Utils.GLModel;
 import Utils.ModelLoaderOBJ;
+import Utils.Rand;
 import Utils.TextureLoader;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.util.texture.Texture;
@@ -22,13 +23,15 @@ public class Asteroids {
     private static int maxAsteroids;
     private float posX, posY, posZ, vecX,vecY,vecZ,rotX,rotY,rotZ;
     private GLModel model;
+    private Texture tex;
 
-    Asteroids(float posX, float posY,float posZ,float vecX,float vecY,float vecZ,float rotX,float rotY,float rotZ, GLModel model)
+    Asteroids(float posX, float posY,float posZ,float vecX,float vecY,float vecZ,float rotX,float rotY,float rotZ, GLModel model, Texture tex)
     {
         this.posX=posX; this.posY=posY; this.posZ=posZ;
         this.vecX=vecX; this.vecY=vecY; this.vecZ=vecZ;
         this.rotX=rotX; this.rotY=rotY; this.rotZ=rotZ;
         this.model=model;
+        this.tex = tex;
     }
 
     public static void initAsteroids(GL2 gl, int numAsteroids){
@@ -50,39 +53,43 @@ public class Asteroids {
                     randX,randY,randZ,
                     0,0,0,
                     0,0,0,
-                    tempModel);
+                    tempModel,loadTextures());
         }
         int j = 0;
 
     }
 
 
-    public static void  loadTextures() {
-        astTex = TextureLoader.loadTexture(
-                new File(Paths.get(".\\Models").toAbsolutePath().normalize().toString() +
-                        "\\Maps\\MoonAsteroid.jpg"));
+    public static Texture  loadTextures() {
+
+            return TextureLoader.loadTexture(
+                    new File(Paths.get(".\\Models").toAbsolutePath().normalize().toString() +
+                            "\\Maps\\AsteroidTex_"+Rand.randInt(0,2)+".jpg"));
+
     }
 
     public static GLModel randomModel(GL2 gl, int rand)
     {
         return ModelLoaderOBJ.LoadModel(
                 Paths.get(".\\Models").toAbsolutePath().normalize().toString()
-                        + "\\Asteroid\\Asteroid_"+rand+".obj",
+                        + "\\Asteroid\\Asteroid_"+Rand.randInt(1,6)+".obj",
                 Paths.get(".\\Models").toAbsolutePath().normalize().toString()
-                        + "\\Asteroid\\Asteroid_"+rand+".mtl",gl);
+                        + "\\Asteroid\\Asteroid_"+Rand.randInt(1,6)+".mtl",gl);
 
     }
 
 
     public static void drawAsteroidField(GL2 gl){
 
-        astTex.enable(gl);
-        astTex.bind(gl);
+
+
         for(int i = 0; i < maxAsteroids; i++) {
+            asteroids[i].tex.enable(gl);
+            asteroids[i].tex.bind(gl);
             gl.glTranslatef(asteroids[i].posX,asteroids[i].posY,asteroids[i].posZ);
             asteroids[i].model.opengldraw(gl);
             gl.glTranslatef(-asteroids[i].posX,-asteroids[i].posY,-asteroids[i].posZ);
+            asteroids[i].tex.disable(gl);
         }
-        astTex.disable(gl);
     }
 }
